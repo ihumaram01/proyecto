2cf955d92e2b1420c3599ab15d8eabc618062b8b
IvanHumara <ihumaram01@educantabria.es>
2025-04-09 20:44:58 +0200

    Update infraestructura.sh

--- scripts/aws/infraestructura.sh
+++ scripts/aws/infraestructura.sh
@@ -94,7 +94,7 @@ aws ec2 authorize-security-group-egress --group-id "$SG_THINLINC_ID" --protocol 
 ###########################################
 
 # Instancia para WireGuard VPN
-INSTANCE_NAME="wireguard-vpn"
+INSTANCE_NAME="VPNWireguard"
 SUBNET_ID="$SUBNET_PUBLIC_ID"
 SECURITY_GROUP_ID="$SG_WIREGUARD_ID"
 PRIVATE_IP="10.0.1.10"
@@ -130,7 +130,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
 echo "${INSTANCE_NAME} creada: ${INSTANCE_ID}"
 
 # Instancia para ThinLinc Agente1
-INSTANCE_NAME="agente1"
+INSTANCE_NAME="ThinLincAgente1"
 PRIVATE_IP="10.0.2.21"
 SECURITY_GROUP_ID="$SG_THINLINC_ID"
 
@@ -146,7 +146,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
 echo "${INSTANCE_NAME} creada: ${INSTANCE_ID}"
 
 # Instancia para ThinLinc Agente2
-INSTANCE_NAME="agente2"
+INSTANCE_NAME="ThinLincAgente2"
 PRIVATE_IP="10.0.2.22"
 
 INSTANCE_ID=$(aws ec2 run-instances \
@@ -161,7 +161,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
 echo "${INSTANCE_NAME} creada: ${INSTANCE_ID}"
 
 # Instancia para ThinLinc Maestro1
-INSTANCE_NAME="maestro1"
+INSTANCE_NAME="ThinLincMaestro1"
 PRIVATE_IP="10.0.2.11"
 
 INSTANCE_ID=$(aws ec2 run-instances \
@@ -176,7 +176,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
 echo "${INSTANCE_NAME} creada: ${INSTANCE_ID}"
 
 # Instancia para ThinLinc Maestro2
-INSTANCE_NAME="maestro2"
+INSTANCE_NAME="ThinLincMaestro2"
 PRIVATE_IP="10.0.2.12"
 
 INSTANCE_ID=$(aws ec2 run-instances \
