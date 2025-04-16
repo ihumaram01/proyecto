31cad5294434e05be2a3b9d7b856852d780a9fec
ihumaram01 <ihumaram01@educantabria.es>
2025-04-16 09:54:56 +0000

    Update infraestructura.sh

--- scripts/aws/infraestructura.sh
+++ scripts/aws/infraestructura.sh
@@ -36,6 +36,25 @@ chmod 400 "${KEY_NAME}.pem"
 echo "Clave SSH creada y almacenada en: ${KEY_NAME}.pem"
 
 ###########################################
+#              BUCKET S3                 #
+###########################################
+
+echo "Creando bucket S3..."
+
+aws s3api create-bucket \
+    --bucket "$BUCKET_NAME" \
+    --region "$REGION" \
+    --create-bucket-configuration LocationConstraint="$REGION" > /dev/null
+
+aws s3api put-public-access-block \
+    --bucket "$BUCKET_NAME" \
+    --public-access-block-configuration \
+        BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true
+
+echo "Bucket S3 creado: $BUCKET_NAME"
+
+
+###########################################
 #                 VPC                     #
 ###########################################
 
