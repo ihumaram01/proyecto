07aa7f522231d37e752ca5618099fcc8884ee0b9
ihumaram01 <ihumaram01@educantabria.es>
2025-04-22 08:05:08 +0000

    Update ldap-cliente.sh

--- scripts/ldap/ldap-cliente.sh
+++ scripts/ldap/ldap-cliente.sh
@@ -11,14 +11,27 @@ dpkg-reconfigure slapd
 
 # Configuración de /etc/ldap/ldap.conf
 echo "Editando /etc/ldap/ldap.conf..."
-echo "BASE      tdc=LDAP,dc=local" | sudo tee -a /etc/ldap/ldap.conf
+echo "BASE      dc=LDAP,dc=local" | sudo tee -a /etc/ldap/ldap.conf
 echo "URI       ldap://10.0.2.30:389" | sudo tee -a /etc/ldap/ldap.conf
+echo "rootbinddn cn=admin,dc=LDAP,dc=local" | sudo tee -a /etc/ldap/ldap.conf
+echo "pam_password md5" | sudo tee -a /etc/ldap/ldap.conf
 
-# Modificación de /etc/nsswitch.conf
+# Configuración de /etc/nsswitch.conf
 echo "Modificando /etc/nsswitch.conf..."
 sed -i 's/^passwd:.*/passwd:     files ldap/' /etc/nsswitch.conf
 sed -i 's/^group:.*/group:      files ldap/' /etc/nsswitch.conf
 sed -i 's/^shadow:.*/shadow:     files ldap/' /etc/nsswitch.conf
+
+# Crear archivo /etc/ldap.secret
+echo "Creando archivo /etc/ldap.secret..."
+echo 'Admin1' | sudo tee /etc/ldap.secret > /dev/null
+sudo chmod 600 /etc/ldap.secret
+sudo chown root:root /etc/ldap.secret
+
+# Reiniciar servicios
+echo "Reiniciando servicios necesarios..."
+sudo systemctl restart nscd
+sudo systemctl restart nslcd || echo "nslcd no está instalado, ignorado"
 
 # Actualizar la base de datos NSS
 echo "Actualizando base de datos NSS..."
