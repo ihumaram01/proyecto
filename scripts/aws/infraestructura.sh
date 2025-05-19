d335f345e2facd193cfc0e2f8fc7408a590582c6
ihumaram01 <ihumaram01@educantabria.es>
2025-05-19 10:30:09 +0200

    Update infraestructura.sh

--- scripts/aws/infraestructura.sh
+++ scripts/aws/infraestructura.sh
@@ -17,7 +17,7 @@ AMI_ID="ami-04b4f1a9cf54c11d0" # Ubuntu Server 24.04
 
 # Tipo de instancia y tamaño del disco
 INSTANCE_TYPE="t3.micro"
-INSTANCE_TYPE_2="t3.small"
+INSTANCE_TYPE_2="t3.medium"
 VOLUME_SIZE=30
 
 # Variables de S3
